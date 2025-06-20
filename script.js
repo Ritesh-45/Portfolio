9ed2181e8f29d2ed5cb56e4768d7de6219c1c26b
swearPants <riteshtikader@gmail.com>
2025-06-21 00:08:52 +0530

    a little effect to the navbar and made the form work

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.addEventListener('mousemove', (e) => {
   spotlight.style.maskImage = `radial-gradient(circle 250px at ${x}px ${y}px, transparent 0%, black 100%)`;
   spotlight.style.webkitMaskImage = `radial-gradient(circle 250px at ${x}px ${y}px, transparent 0%, black 100%)`;
 });
+
+let navbar = document.querySelector('.navbar');
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    }
+  })
+}
